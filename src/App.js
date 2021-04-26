0aa4983a29dbe1629445025f1a060005d878436a
Sagartare12 <sagartare2014@gmail.com>
2021-04-26 11:03:12 +0530

    This is first commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Board from './components/Board'
 export default () => (
   <div>
-    <h1>Tic Tac Toe</h1>
+    <h1>Tic Tac Toe game</h1>
    <Board />
   </div>
 );
