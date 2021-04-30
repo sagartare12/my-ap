4f8fde809debaf87ddb8abe4b2703c20058ebd80
Sagartare12 <sagartare2014@gmail.com>
2021-04-30 14:29:14 +0530

    Calculate the winner and lift up the state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Board from './components/Board'
+import {calculateWinner} from './helpers'
 import "./styles/root.scss"
-export default () => (
+const   App =() => {
+
+  const [board, setBoard] = useState(Array(9).fill(null));
+
+const [isXNext , setIsXNext ] = useState(false);
+
+const winner = calculateWinner(board);
+console.log(winner);
+
+const  message = winner 
+ ? `Winner is ${winner}`
+ : `Next player is ${isXNext ? 'X' : 'O'}`;
+const handlerSquareClick = (position)=> {
+     if( board[position] || winner) {
+         return ;
+     }
+
+    setBoard( (prev) =>{
+        return prev.map((square ,pos) => {
+            if(pos === position) {
+                return   isXNext ? 'X' : 'O';
+            }
+            return square;
+           
+        })
+    })
+  setIsXNext( prev => !prev);
+};
+  return (
   <div className="app">
     <h1>Tic Tac Toe game</h1>
-   <Board />
+    <h1>{message}</h1>
+   <Board board={board} handlerSquareClick={handlerSquareClick} />
   </div>
-);
+  );
+};
+export default App
